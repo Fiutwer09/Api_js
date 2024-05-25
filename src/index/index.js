62ec8ea539761625b4dfed381c01bf6caf35838f
camilo <camilocardona2617@gmail.com>
2024-05-25 10:44:44 -0500

    producto final

--- src/index/index.js
+++ src/index/index.js
@@ -4,6 +4,8 @@ const createError = require('http-errors');
 
 const { Response } = require('../common/response');
 
+
+
 module.exports.IndexAPI =(app) => {
     const router = express.Router();
     router.get("/", (req,res)=>{const menu ={
